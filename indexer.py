ab6f2248f4e380236299bf532ad2af83d74a9b14
kuriyashreyansh <kuriyashreyansh@gmail.com>
2026-06-25 00:58:18 +0530

    Day 2: Document class working, fixed path/encoding issues
    Signed-off-by: kuriyashreyansh <kuriyashreyansh@gmail.com>

---
+++ indexer.py
@@ -0,0 +1,10 @@
+stored_documents=[]
+
+
+def add_document(document):
+    stored_documents.append(document)
+    print("\nAdded in ur documents!!!")
+
+
+def search():
+    print("Search feature maybe coming sooon!!!")
---
+++ model.py
@@ -0,0 +1,18 @@
+class Document:
+    def __init__(self,Id,title,text):
+        self.Id=Id
+        self.title=title
+        self.text=text
+
+    def __repr__(self):
+        return f'{self.Id}. {self.title}\n\n{self.text}\n'
+
+import os
+docs=[]
+for i,j in enumerate(os.listdir("data")):
+    with open(f'data\{j}') as f:
+        content=f.read()
+    docs.append(Document(i+1,j,content))
+
+for d in docs:
+    print(d)
