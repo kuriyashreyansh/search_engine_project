25433fe9092ee037074360094114f65c28f95192
kuriyashreyansh <kuriyashreyansh@gmail.com>
2026-06-27 23:59:52 +0530

    Day 5: started making function of search which is initially for single word parameter check
    Signed-off-by: kuriyashreyansh <kuriyashreyansh@gmail.com>

--- indexer.py
+++ indexer.py
@@ -1,9 +1,11 @@
 from db import get_connection
+from tokenizer import tokenize
 
 class Indexer:
 
     def __init__(self):
         self.documents=[]
+
 
     def add_document(self,document):
         conn=get_connection()
@@ -17,8 +19,31 @@ class Indexer:
         conn.close()
         print("\nAdded in ur documents!!!")
 
+
     def search(self,query):
+        query_token=tokenize(query)
         print("Search feature maybe coming sooon!!!")
+        
+    def get_term_id(self,word):
+        conn=get_connection()
+        cur=conn.cursor()
+        #as it is single word earlier hence i am not adding tuple format
+        cur.execute("SELECT term_id FROM terms WHERE word = %s",(word,))
+        match=cur.fetchone()
+        if match is None:
+            print("Query word not found")
+            conn.commit()
+            cur.close()
+            conn.close()
+            return None
+        else:
+            cur.execute("select (doc_id,term_frequency) from postings where word=%s",(match))
+            conn.commit()
+            cur.close()
+            conn.close()
+            return match
+
+
 
 # import os
 # from model import Document
