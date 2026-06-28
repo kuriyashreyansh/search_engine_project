d4510f0a0011c0622e9691a6aab41ddf5b06a8bb
kuriyashreyansh <kuriyashreyansh@gmail.com>
2026-06-28 18:42:39 +0530

    search() working end to end: tokenizes query, ranks by frequency, shows titles
    Signed-off-by: kuriyashreyansh <kuriyashreyansh@gmail.com>

--- indexer.py
+++ indexer.py
@@ -22,7 +22,16 @@ class Indexer:
 
     def search(self,query):
         query_token=tokenize(query)
-        print("Search feature maybe coming sooon!!!")
+        search_result=self.get_term_id(list(query_token.keys())[0])
+        if search_result is None:
+            print("No results found")
+            return None
+        else:
+            sorted_items = sorted(search_result, key=lambda item: item[1], reverse=True)
+            for doc_id,frequency in sorted_items:
+                title=self.get_document_title(doc_id)
+                print(f"{query} word is found in {title} {frequency} times")
+        # return sorted_result
         
     def get_term_id(self,word):
         conn=get_connection()
@@ -32,7 +41,7 @@ class Indexer:
         if match is None:
             cur.close()
             conn.close()
-            return "Query word not found"
+            return None
         else:
             cur.execute("select doc_id,term_frequency from postings where term_id=%s",(match[0],))
             fetched_data=cur.fetchall()
@@ -41,11 +50,18 @@ class Indexer:
             conn.close()
             return fetched_data
         
+
+    def get_document_title(self,doc_id):
+        conn=get_connection()
+        cur=conn.cursor()
+        cur.execute("select title from documents where id=%s",(doc_id,))
+        document_title=cur.fetchone()[0]
+        cur.close()
+        conn.close()
+        return document_title
+
 indexer=Indexer()
-print(indexer.get_term_id("learning"))
-print(indexer.get_term_id("xyzabc"))
-
-
+indexer.search("learning")
 
 # import os
 # from model import Document
--- tokenizer.py
+++ tokenizer.py
@@ -2,8 +2,6 @@ import re
 
 def tokenize(text):
     tokens=re.findall(r'\w+',text.lower())
-
-
     token_dict={}
     # here we had done the change of value means at 1st coming it is coming value 1 then after another same token value changes to 2 and also it is 0 for token not present
     for token in tokens:
