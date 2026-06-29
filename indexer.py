6b6610333a003c85a8865bc7bff0478973375b9b
kuriyashreyansh <kuriyashreyansh@gmail.com>
2026-06-29 22:42:29 +0530

    Multi-word search working: combines scores across query words, skips missing words
    Signed-off-by: kuriyashreyansh <kuriyashreyansh@gmail.com>

--- indexer.py
+++ indexer.py
@@ -19,18 +19,19 @@ class Indexer:
         conn.close()
         print("\nAdded in ur documents!!!")
 
+    # Single Search query
 
-    def search(self,query):
-        query_token=tokenize(query)
-        search_result=self.get_term_id(list(query_token.keys())[0])
-        if search_result is None:
-            print("No results found")
-            return None
-        else:
-            sorted_items = sorted(search_result, key=lambda item: item[1], reverse=True)
-            for doc_id,frequency in sorted_items:
-                title=self.get_document_title(doc_id)
-                print(f"{query} word is found in {title} {frequency} times")
+    # def search(self,query):
+    #     query_token=tokenize(query)
+    #     search_result=self.get_term_id(list(query_token.keys())[0])
+    #     if search_result is None:
+    #         print("No results found")
+    #         return None
+    #     else:
+    #         sorted_items = sorted(search_result, key=lambda item: item[1], reverse=True)
+    #         for doc_id,frequency in sorted_items:
+    #             title=self.get_document_title(doc_id)
+    #             print(f"{query} word is found in {title} {frequency} times")
         # return sorted_result
         
     def get_term_id(self,word):
@@ -59,14 +60,25 @@ class Indexer:
         cur.close()
         conn.close()
         return document_title
+    
+# MULTIPLE WORD SEARCH handling
+
+
+    def new_search(self,query):
+        query_tokens=tokenize(query)
+        summurised_data={}
+        for i in query_tokens.keys():
+            term_id_frequency=self.get_term_id(i)
+            if term_id_frequency is None:
+                    pass
+            else:
+                for j,k in term_id_frequency:
+                    summurised_data[j]=summurised_data.get(j,0)+k
+        sorted_summrised_data=sorted(summurised_data.items(),key=lambda i : i[1],reverse=True)
+        for i,j in sorted_summrised_data:
+            print(f"{self.get_document_title(i)} file has {query} {j} times in it")
+        
+
 
 indexer=Indexer()
-indexer.search("learning")
-
-# import os
-# from model import Document
-# indexer=Indexer()
-# for i ,j in enumerate(os.listdir("data")):
-#     with open(os.path.join("data",j),encoding='utf-8') as f:
-#         doc_data=f.read()
-#     indexer.add_document(Document(i,j,doc_data))
+indexer.new_search("machine xyzabc")
