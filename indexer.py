27b9e4fbfc80145876e36fb7b3f0dfc17cf6d2a4
kuriyashreyansh <kuriyashreyansh@gmail.com>
2026-07-02 01:44:38 +0530

    Tech debt: single shared connection on Indexer instead of per-method connections
    Signed-off-by: kuriyashreyansh <kuriyashreyansh@gmail.com>

--- indexer.py
+++ indexer.py
@@ -6,76 +6,62 @@ class Indexer:
 
     def __init__(self):
         self.documents=[]
-
+        self.conn=get_connection()
 
     def add_document(self,document):
-        conn=get_connection()
         self.documents.append(document)
-        cur=conn.cursor()
+        cur=self.conn.cursor()
         cur.execute("insert into documents(title,content) values(%s,%s) returning Id",(document.title,document.text))
         new_id=cur.fetchone()[0]
         document.Id=new_id
-        conn.commit()
+        self.conn.commit()
         cur.close()
-        conn.close()
         print("\nAdded in ur documents!!!")
 
         
     def get_term_id(self,word):
-        conn=get_connection()
-        cur=conn.cursor()
+        cur=self.conn.cursor()
         cur.execute("SELECT term_id FROM terms WHERE word = %s",(word,))
         match=cur.fetchone()
         if match is None:
             cur.close()
-            conn.close()
             return None
         else:
             cur.execute("select doc_id,term_frequency from postings where term_id=%s",(match[0],))
             fetched_data=cur.fetchall()
-            conn.commit()
             cur.close()
-            conn.close()
             return (match[0],fetched_data)
         
 
     def get_document_title(self,doc_id):
-        conn=get_connection()
-        cur=conn.cursor()
+        cur=self.conn.cursor()
         cur.execute("select title from documents where id=%s",(doc_id,))
         document_title=cur.fetchone()[0]
         cur.close()
-        conn.close()
         return document_title
     
 
     def get_document_count(self,doc_id):
-        conn=get_connection()
-        cur=conn.cursor()
+        cur=self.conn.cursor()
         cur.execute("Select sum(term_frequency) from postings where doc_id=%s",(doc_id,))
         frequency=cur.fetchone()[0]
         cur.close()
-        conn.close()
         return frequency
     
 
     def get_total_documents(self):
-        conn=get_connection()
-        cur=conn.cursor()
+        cur=self.conn.cursor()
         cur.execute("select count(id) from documents")
         total_documents=cur.fetchone()[0]
         cur.close()
-        conn.close()
         return total_documents
     
 
     def get_document_frequency(self,term_id):
-        conn=get_connection()
-        cur=conn.cursor()
+        cur=self.conn.cursor()
         cur.execute("select count(*) from postings where term_id = %s",(term_id,))
         doc_freq=cur.fetchone()[0]
         cur.close()
-        conn.close()
         return doc_freq
     
 
@@ -83,7 +69,7 @@ class Indexer:
         return math.log(self.get_total_documents()/self.get_document_frequency(term_id))
     
 
-# Multi-word search with TF-IDF ranking
+    # Multi-word search with TF-IDF ranking
 
     def new_search(self,query):
         query_tokens=tokenize(query)
@@ -99,17 +85,3 @@ class Indexer:
         sorted_summrised_data=sorted(summurised_data.items(),key=lambda i : i[1],reverse=True)
         for i,j in sorted_summrised_data:
             print(f"{self.get_document_title(i)} file has {query} Relevance score : {j}")
-        
-
-        
-    
-    
-
-    def get_document_frequency(self,term_id):
-        conn=get_connection()
-        cur=conn.cursor()
-        cur.execute("select count(*) from postings where term_id = %s",(term_id,))
-        doc_freq=cur.fetchone()[0]
-        cur.close()
-        conn.close()
-        return doc_freq
